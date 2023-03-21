4f9ed74a8269b11cd99794323f7a9b1583c92fa2
Callum Casey <123276837+RatherSlow@users.noreply.github.com>
2023-03-21 13:55:05 +0000

    CCTV
    Fixed High Score Menu, Created CCTV model and started on script (got it rotating)

---
+++ Assets/Enemy/Scripts/CCTV.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTV : MonoBehaviour
+{
+    [SerializeField]
+    float CameraAngle = -35f;
+    float rotateY = 0f;
+    [SerializeField]
+    float spinspeed = 0.3f;
+    [SerializeField]
+    bool isActive = true;
+    public Transform CameraCage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive == true)
+        {
+            if (rotateY > 50f || rotateY < -50f)
+            {
+                spinspeed = -1*spinspeed;
+                Spin();
+            }
+            else
+            {                
+                Spin();
+            }
+        }
+    }
+    public void Spin()
+    {
+        CameraCage.localRotation = Quaternion.Euler(CameraAngle, rotateY, 0f);
+        rotateY += spinspeed;
+    }
+}
